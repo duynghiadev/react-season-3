f2aa75e7666aaac247fc68cf51c7417bef279c04
duynghia-123 <duynghia22302@gmail.com>
2022-11-05 02:23:58 +0700

    Use variable with React & useState Hook

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,25 +1,42 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 
-class TodoList extends Component {
-  // khai báo state
-  state = {
-    name: "Eric",
-  };
+// class TodoList extends Component {
+//   // khai báo state
+//   state = {
+//     name: "",
+//   };
 
-  // gán lại giá trị cho state
-  render() {
-    return (
-      <div>
-        <label>Name</label>
-        <input
-          type="text"
-          onChange={(event) => this.setState({ name: event.target.value })}
-        />
-        <br />
-        <br />
-        Hello Todo List name = {this.state.name}
-      </div>
-    );
-  }
-}
+//   // gán lại giá trị cho state
+//   render() {
+//     return (
+//       <div>
+//         <label>Name</label>
+//         <input
+//           type="text"
+//           onChange={(event) => this.setState({ name: event.target.value })}
+//         />
+//         <br />
+//         <br />
+//         Hello Todo List name = {this.state.name}
+//       </div>
+//     );
+//   }
+// }
+
+const TodoList = () => {
+  const [name, setName] = useState("");
+
+  return (
+    <div className="">
+      <label htmlFor="">Name</label>
+      <input
+        type="text"
+        value={name}
+        onChange={(event) => setName(event.target.value)}
+      />
+      <br /> <br />
+      Hello Todo List name = {name}
+    </div>
+  );
+};
 export default TodoList;
