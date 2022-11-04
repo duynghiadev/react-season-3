fa5a8a2f096514722c5b06b505a957210ad03622
duynghia-123 <duynghia22302@gmail.com>
2022-11-04 14:20:36 +0700

    project structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
+const App = () => {
+  // function component
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +19,6 @@ function App() {
       </header>
     </div>
   );
-}
+};
 
 export default App;
