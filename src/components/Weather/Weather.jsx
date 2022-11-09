bea97f49fc8d684607cced52aefb7aaeb32c3353
duynghia-123 <duynghia22302@gmail.com>
2022-11-10 02:41:52 +0700

    Lifecycle Methods - UseEffect Hook

--- src/components/Weather/Weather.jsx
+++ src/components/Weather/Weather.jsx
@@ -3,20 +3,39 @@ import axios from "axios";
 
 const Weather = () => {
   const [title, setTitle] = useState("");
-  useEffect(async () => {
-    console.log(">>> run use effect");
-    let response = await axios({
-      method: "post",
-      url: "https://test-react-hook-season-3.herokuapp.com/get-data-by-url",
-      data: { url: "https://www.metaweather.com/api/location/1236594/" },
-    });
-    console.log(">>> check data: ", response.data);
 
-    setTimeout(() => {
-      setTitle(response.data.title);
-    }, 1000);
+  // useEffect(async () => {
+  //   console.log(">>> run use effect");
+  //   let response = await axios({
+  //     method: "post",
+  //     url: "http://localhost:8080/get-data-by-url",
+  //     data: { url: "https://www.metaweather.com/api/location/1236594/" },
+  //   });
+  //   console.log(">>> check data: ", response.data);
+
+  //   setTimeout(() => {
+  //     setTitle(response.data.title);
+  //   }, 1000);
+  // }, []);
+
+  useEffect(() => {
+    async function fetchData() {
+      console.log(">>> run use effect");
+      let response = await axios({
+        method: "post",
+        url: "http://localhost:8080/get-data-by-url",
+        data: { url: "https://www.metaweather.com/api/location/1236594/" },
+      });
+      console.log(">>> check data: ", response.data);
+
+      setTimeout(() => {
+        setTitle(response.data.title);
+      }, 1000);
+    }
+    fetchData();
   }, []);
 
+  console.log(">>> Thứ tự Run Code <<<");
   console.log(">> run render");
   return <div> inside weather component: title = {title}</div>;
 };
