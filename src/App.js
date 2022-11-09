dbf805e69aa06947c22f288e84ce779e4bb15711
duynghia-123 <duynghia22302@gmail.com>
2022-11-09 08:26:10 +0700

    react routers (v5)

--- src/App.js
+++ src/App.js
@@ -4,30 +4,38 @@ import Home from "./components/Home";
 import AddNewProduct from "./components/AddNewProduct";
 import Product from "./components/Products/Product";
 import "react-image-lightbox/style.css";
+import Nav from "./components/Navigation/Nav";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-// function App() {
 const App = () => {
-  const x = [1, 2, 3, { name: "eric" }];
-  // const x = { name: 'eric' }
-
   return (
-    <div className="App">
-      <header className="App-header content-left">
-        {/* inline */}
-        <div style={{ textAlign: "center" }}>
-          <img src={logo} className="App-logo" alt="logo" />
-        </div>
-        <p>
-          <span>Hello world React</span> with DuyNghia
-        </p>
-        <Home />
-      </header>
-      <div className="content-right">
-        <AddNewProduct />
-        <hr />
-        <Product />
-      </div>
-    </div>
+    <Router>
+      <Nav />
+      <Switch>
+        <Route path="/about">
+          <Product />
+        </Route>
+        <Route path="/users">{/* <Users /> */}</Route>
+        <Route path="/">
+          <div className="App">
+            <header className="App-header content-left">
+              <div style={{ textAlign: "center" }}>
+                <img src={logo} className="App-logo" alt="logo" />
+              </div>
+              <p>
+                <span>Hello world React</span> with Boo
+              </p>
+              <Home />
+            </header>
+            <div className="content-right">
+              <AddNewProduct />
+              <hr />
+              <Product />
+            </div>
+          </div>
+        </Route>
+      </Switch>
+    </Router>
   );
 };
 
